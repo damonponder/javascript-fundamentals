7a6a73464e78b16eb4b8288683cc39c3f86a3e92
damonponder <damonponder@MacBook-Pro.local>
2026-04-10 12:25:36 -0500

    Initialized JavaScript fundamentals repository

---
+++ READM.md
@@ -0,0 +1,10 @@
+# JavaScript Fundamentals
+
+This repository contains focused exercises on core JavaScript concepts used in frontend development and interviews.
+
+## Topics
+- Arrays
+- Objects
+- Functions
+- Closures
+- Async JavaScript
---
+++ js_practice.js
@@ -0,0 +1,24 @@
+let numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+[1, 2, 3, 4, 5].forEach(num => {
+console.log(num_each);
+});
+
+let doubled = numbers1.map(function(num){
+    return num * 2
+});
+console.log(doubled);
+
+let evens = numbers1.filter(function(num){
+    return num % 2 === 0
+});
+console.log(evens);
+
+let doubled2 = numbers1.map(num => num * 3);
+console.log(doubled2);
+
+let numbers_greater = numbers1.filter(num => num > 3);
+
+let double_numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => num * 2);
+
+let 
